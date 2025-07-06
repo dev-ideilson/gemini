c4d6beb71bd824826fc5642ee7d412a8823f2941
TWtutoriais <magnusrevil748@gmail.com>
2025-07-06 05:17:13 -0300

    Dashbord responsivo

--- web/src/layout/AppFooter.vue
+++ web/src/layout/AppFooter.vue
@@ -1,9 +1,11 @@
 <script setup></script>
 
 <template>
-    <div class="layout-footer">
-        AI PRO by
-        <a href="https://github.com/dev-ideilson" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Ideilson</a>
-        <a href="https://github.com/TWtutoriais" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Tiago</a>
+    <div class="layout-footer fixed  bottom-0 left-0 right-0 bg-[#e6ebf3] w-full">
+        <div class="flex item-center justify-center gap-4">
+            <p class="text-sm">AI PRO by</p>
+            <a href="https://github.com/dev-ideilson" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Ideilson</a>
+            <a href="https://github.com/TWtutoriais" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Tiago</a>
+        </div>
     </div>
 </template>
--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="flex flex-col space-y-5 items-center justify-center">
-        <!-- <div class="w-10/12 h-auto flex flex-col item-center justify-center p-2 bg-[#444444] rounded-md">
-             <h1 class="text-white text-2xl">Titulo</h1>
-             <p class="text-white">Resposta</p>
-        </div> -->
+    <div class="flex flex-col space-y-5 items-center justify-center mb-60 overflow-x-hidden">
 
-
-        <Panel class="mx-12">
+        <!-- Painel IA -->
+        <Panel class="w-full max-w-4xl mx-4">
             <template #header>
                 <div class="flex items-center gap-2">
                     <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
@@ -15,77 +11,67 @@
             </template>
             <template #footer>
                 <div class="flex flex-wrap items-center justify-between gap-4">
-                    <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
-                    <div class="flex item-center p-4">
+                    <span class="text-surface-500 dark:text-surface-400">Resposta: 06/07/2024 às 04:03</span>
+                    <div class="flex items-center p-4">
                         <Button label="Copiar" />
                     </div>
                 </div>
             </template>
             <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet...
             </p>
-
         </Panel>
 
-        <Panel class="mx-12">
+        <!-- Painel Usuário -->
+        <Panel class="w-full max-w-4xl mx-4">
             <template #header>
-                <div class="flex items-center gap-2">
+                <div class="w-full flex flex-row-reverse items-center gap-2">
                     <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
                     <span class="font-bold">Usuário</span>
                 </div>
             </template>
             <template #footer>
                 <div class="flex flex-wrap items-center justify-between gap-4">
-                    <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
+                    <span class="text-surface-500 dark:text-surface-400">Pergunta: 06/07/2024 às 04:03</span>
                 </div>
             </template>
             <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet...
             </p>
-
         </Panel>
 
+        <!-- Input fixo -->
+        <div
+            class="w-full max-w-4xl min-h-32 flex flex-col justify-center rounded-2xl shadow-md fixed bottom-20 p-5 bg-[#e6ebf3] mx-4">
+            <FloatLabel variant="on" class="mt-5">
+                <Textarea id="on_label" v-model="CheckText" class="w-full resize-none max-h-40" autoResize rows="1" />
+                <label for="on_label">Pergunte-me alguma coisa.</label>
+            </FloatLabel>
 
-       <div class="w-10/12 min-h-32 flex flex-col justify-center rounded-2xl shadow-md fixed bottom-20 p-5 bg-[#e6ebf3]">
-    <FloatLabel variant="on" class="mt-5">
-        <Textarea id="on_label" v-model="CheckText" class="w-full resize-none max-h-40" autoResize rows="1" />
-        <label for="on_label">Pergunte-me alguma coisa.</label>
-    </FloatLabel>
-
-    <div class="w-full flex items-center justify-between p-4">
-        <div class="flex items-center justify-center px-2 gap-4">
-            <span class="pi pi-plus text-4xl hover:cursor-pointer"></span>
-            <span class="pi pi-sliders-h text-4xl hover:cursor-pointer"></span>
-            <p>Ferramentas</p>
+            <div class="w-full flex items-center justify-between p-4">
+                <div class="flex items-center justify-center px-2 gap-4">
+                    <span class="pi pi-plus text-4xl hover:cursor-pointer"></span>
+                    <span class="pi pi-sliders-h text-4xl hover:cursor-pointer"></span>
+                    <p>Ferramentas</p>
+                </div>
+                <div class="flex items-center p-2 gap-4">
+                    <span
+                        class="pi pi-microphone flex items-center justify-center w-10 h-10 hover:cursor-pointer text-4xl bg-[#bcbcca] rounded-full"></span>
+                    <span :class="isTextPresent ? 'pi pi-send' : 'pi pi-microchip-ai'"
+                        class="flex items-center justify-center w-10 h-10 hover:cursor-pointer text-4xl bg-[#bcbcca] rounded-full"></span>
+                </div>
+            </div>
         </div>
-        <div v-if="(Status = true)" class="flex item-center p-2 gap-4">
-            <span class="pi pi-microphone flex items-center justify-center w-10 h-10 hover:cursor-pointer text-4xl bg-[#bcbcca] rounded-full"></span>
-            <span :class="isTextPresent ? 'pi pi-send' : 'pi pi-microchip-ai' " class="flex items-center justify-center w-10 h-10 hover:cursor-pointer text-4xl bg-[#bcbcca] rounded-full"></span>
-        </div>
-    </div>
-</div>
-
     </div>
 </template>
+
 
 <script setup>
 import { computed, ref } from 'vue';
 
-const CheckText = ref(''); 
+const CheckText = ref('');
 
-const isTextPresent = computed(() => CheckText.value.trim().length >0);
+const isTextPresent = computed(() => CheckText.value.trim().length > 0);
 
 </script>
 
