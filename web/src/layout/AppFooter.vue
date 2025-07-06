22979f711305548f60f26a34d99ab29041d15ae1
Ideilson Souza <ideilson.raise@gmail.com>
2025-07-06 00:54:59 -0300

    START PROJECT

---
+++ web/src/layout/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        AI PRO by
+        <a href="https://github.com/dev-ideilson" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Ideilson</a>
+        <a href="https://github.com/TWtutoriais" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Tiago</a>
+    </div>
+</template>
